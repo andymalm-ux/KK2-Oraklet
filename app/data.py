9a6709f32c860935de8f49e82a1efabae1ae0a6b
andymalm-ux <andreas.malmgren@student.nbi-handelsakademin.se>
2026-06-03 11:08:10 +0200

    Added logic to store data in memory to be able to fetch uploaded data. Added a get endpoint to fetch data stored in memory. Added error messages for wrong file format and if no data is stored in memory.

--- app/data.py
+++ app/data.py
@@ -1,20 +1,34 @@
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, HTTPException, UploadFile
 import csv
 import io
 
 router = APIRouter()
 
+csv_data = []
+
 @router.post("/upload")
 async def upload_csv(file: UploadFile = File(...)):
+  global csv_data
   
   if file.filename and file.filename.endswith('.csv'):
     content = await file.read()
 
-    csv_data = io.StringIO(content.decode('utf-8'))
-    csv_reader = csv.DictReader(csv_data)
-    json_data = [row for row in csv_reader]
+    csv_decode = io.StringIO(content.decode('utf-8'))
+    csv_reader = csv.DictReader(csv_decode)
+    # json_data = [row for row in csv_reader]
+    csv_data = list(csv_reader)
 
-    return json_data
+    return {"message": "File successfully uploaded"}, csv_data
   
   else:
-    return {"error": "Only csv files are allowed."}
+    raise HTTPException(status_code=415, detail="You can only upload csv files")
+  
+@router.get("/data")
+async def get_data():
+  
+  get_stored_data = csv_data
+
+  if len(get_stored_data) == 0 :
+    raise HTTPException(status_code=404, detail="You need to upload a csv file first.")
+  else:
+    return get_stored_data
