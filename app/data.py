190e561fea77a806736311c32cd9fe18fb016762
andymalm-ux <andreas.malmgren@student.nbi-handelsakademin.se>
2026-06-04 08:00:32 +0200

    Add statistics endpoint to retrieve data summary from uploaded CSV

--- app/data.py
+++ app/data.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, File, HTTPException, UploadFile
 import csv
 import io
+import pandas as pd
 
 router = APIRouter()
 
@@ -14,8 +15,7 @@ async def upload_csv(file: UploadFile = File(...)):
     content = await file.read()
 
     csv_decode = io.StringIO(content.decode('utf-8'))
-    csv_reader = csv.DictReader(csv_decode)
-    # json_data = [row for row in csv_reader]
+    csv_reader = csv.DictReader(csv_decode)    
     csv_data = list(csv_reader)
 
     return {"message": "File successfully uploaded"}, csv_data
@@ -32,3 +32,14 @@ async def get_data():
     raise HTTPException(status_code=404, detail="You need to upload a csv file first.")
   else:
     return get_stored_data
+  
+@router.get ("/data/stats")
+async def get_data_stats():
+  
+  get_data_stats = csv_data
+
+  if len(get_data_stats) == 0 :
+    raise HTTPException(status_code=404, detail="You need to upload a csv file first.")
+  else:
+    stats = pd.DataFrame(get_data_stats)
+    return stats.describe().to_dict()
