a1c914333c93f0f274edeced6aa72aa8f5b7f49d
andymalm-ux <andreas.malmgren@student.nbi-handelsakademin.se>
2026-06-04 12:29:48 +0200

    Refactor steps module to integrate DataFramePromptBuilder and update AI ask endpoint

--- app/chain/steps.py
+++ app/chain/steps.py
@@ -1,7 +1,8 @@
 from transformers import pipeline
 from pydantic import BaseModel, PrivateAttr
 from app.chain.runnable import Runnable
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+import pandas as pd
 
 router = APIRouter()
 
@@ -12,8 +13,35 @@ class PromptBuilder(Runnable[PromptInput, str]):
   def invoke(self, data: PromptInput) -> str:
     return data.question
   
+class QuestionInput(BaseModel):
+  question: str
+
+class DataFramePromptBuilder(Runnable[DataFrameContext, str]):
+  def invoke(self, data: DataFrameContext) -> str:
+
+      df = pd.DataFrame(data.csv_data)
+
+      return f"""
+Du är en assistent som analyserar CSV-data.
+
+Regler:
+- Använd endast information från tabellen.
+- Hitta inte på information.
+- Om svaret inte finns i tabellen ska du säga det.
+- Om användaren ber om innehållet i tabellen ska du visa raderna.
+
+Tabell:
+
+{df.head(10).to_string()}
+
+Fråga:
+{data.question}
+
+Svar:
+"""
+  
 class SmolLM(Runnable[str, str]):
-    model_name: str = "HuggingFaceTB/SmolLM-135M-Instruct"
+    model_name: str = "HuggingFaceTB/SmolLM2-360M-Instruct"
 
     _pipe = PrivateAttr()
 
@@ -41,10 +69,24 @@ class Parser(Runnable[str, Answer]):
     answer = data.split("Svar: ")[-1].strip()
     return Answer(answer=answer)
   
-chain = (PromptBuilder() | SmolLM() | Parser())
+chain = (DataFramePromptBuilder() | SmolLM() | Parser())
 
-@router.post("/AI/ask", response_model=Answer)
-def ask(question: PromptInput):
-  result = chain.invoke(question)
+# @router.post("/AI/ask", response_model=Answer)
+# def ask(question: DataFrameContext):
+#   result = chain.invoke(question)
 
-  return result
+#   return result
+
+@router.post("/AI/ask")
+def ask(
+    question: QuestionInput,
+    request: Request
+):
+    csv_data = request.app.state.csv_data
+
+    context = DataFrameContext(
+        question=question.question,
+        csv_data=csv_data
+    )
+
+    return chain.invoke(context)
